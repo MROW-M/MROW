e1d31c12aebc8db236444b29e8647acb86320c91
Mohammad Mahdi Yari <mohammadmahdiyari3072@gmail.com>
2025-10-30 23:27:27 +0330

    Add files via upload

---
+++ T.daneshgah. 4/soal 2 .cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int position = -1; // to store the position of number 2 if found
+        Console.WriteLine("Enter 5 numbers:");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Number {i}: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 2)
+            {
+                position = i;
+                break; // stop reading if 2 is found
+            }
+        }
+
+        Console.WriteLine();
+
+        if (position != -1)
+            Console.WriteLine($"Number 2 was found at position {position}.");
+        else
+            Console.WriteLine("Number 2 was not found among the entered numbers.");
+    }
+}
